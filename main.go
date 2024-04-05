758a509296dfaabfeab093e13de5c19bb9fe8b91
Tenesh Vignesan <teneshdev@gmail.com>
2024-04-05 23:42:10 +0800

    wip

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type contact struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contact
+}
 
 func main() {
-	newCards := newDeckFromFile("my_cards")
 
-	newCards.shuffle();
-	fmt.Println(newCards)
+	alex := person{
+		firstName:"Alex", 
+		lastName: "Anderson",
+		contact: contact{
+			email: "alex@test.com",
+			zipCode: 8000,
+		},
+	}
+
+	alex.updateName("Jimmy")
+	alex.print()
+}
+
+func (p person) print(){
+	fmt.Printf("%+v",p)
+}
+
+func (p *person) updateName(newFirstName string){
+	p.firstName = newFirstName
 }
